d93c6ed4a57350926b102f651e01a706eec4c86a
Clever Cottonmouth <mirajhadish48@gmail.com@users.noreply.github.com>
2025-06-24 12:30:53 +0530

    feat: Implement navigation to dashboard based on user role
    - Updated app.component.html to make the application title clickable, redirecting users to the dashboard.
    - Enhanced app.component.ts by adding a goToDashboard method that navigates users to different dashboard routes based on their role (Librarian or Student), with a fallback to the login page for unauthorized access.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from './shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,16 @@ import { AuthService } from './shared/services/auth.service';
 export class AppComponent {
   title = 'LibraryManagement.WEB';
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
+
+  goToDashboard() {
+    const role = this.authService.getRole();
+    if (role === 'Librarian') {
+      this.router.navigate(['/librarian/dashboard']);
+    } else if (role === 'Student') {
+      this.router.navigate(['/student/dashboard']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
 }
